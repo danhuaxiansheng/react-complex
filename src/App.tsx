7583c978c8afe9a100cdb11afd2936aa2608bd07
zhang.haipeng <646042371@qq.com>
2023-05-29 17:18:51 +0800

    【*】删除多余代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 
 import { Routes, Route } from "react-router-dom";
 // import { ConfigProvider } from 'antd';
